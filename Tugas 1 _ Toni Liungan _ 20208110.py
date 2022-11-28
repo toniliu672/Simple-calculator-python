4c11be90fe6db0f8b22561a3829e37fa9166d346
Toni Liungan <toniliu672@gmail.com>
2022-11-29 02:26:02 +0800

    feet: init calculator python

---
+++ Tugas 1 _ Toni Liungan _ 20208110.py
@@ -0,0 +1,29 @@
+print("""
+Calculator """)
+while True:
+    try:
+        angka1 = float(input("Angka pertama : "))
+        angka2 = float(input("Angka kedua : "))
+        break
+    except ValueError:
+        print("""Masukkan Angka!
+        """)
+
+operasi = input("Operasi : ")
+if operasi == 'x':
+    result = angka1 * angka2
+elif operasi == '/':
+    result = angka1 / angka2
+elif operasi == '+':
+    result = angka1 + angka2
+elif operasi == '-':
+    result = angka1 - angka2
+elif operasi == '^':
+    result = angka1 ** angka2
+elif operasi == '?':
+    result = (angka1 + 5) - (angka2 - 1)
+
+else : 
+    print("pilih operasi yang sesuai")
+
+print(f"hasil yang diperoleh adalah {result}")
